543ab0b25e177a1754775da4054b438fdbd58ee7
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-09-12 20:14:59 +0300

    Update ContactList.tsx

--- src/components/contact-list/ContactList.tsx
+++ src/components/contact-list/ContactList.tsx
@@ -1,38 +1,32 @@
-import { Component } from 'react';
-import type { StateProp } from '../app/App';
+import type { Contact, ContactDeleteProp } from '../app/App';
 
-class ContactList extends Component<StateProp> {
-  render() {
-    const {
-      state: { contacts, filter = '' },
-    } = this.props;
-    return (
-      <ul>
-        {contacts
-          .filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase()))
-          .map(({ id, name, number }) => {
-            return (
-              <li key={id} className='flex gap-8'>
-                <span className='pb-3'>
-                  {name}: {number}
-                </span>
-                <button
-                  className='m-1 bg-[#696969] px-1 text-white hover:bg-black'
-                  type='button'
-                  onClick={() => this.props.contactDelete(id)}
-                >
-                  Delete
-                </button>
-              </li>
-            );
-          })}
-      </ul>
-    );
-  }
-}
-
-// const ContactList = () => {
-//   return <span>list</span>;
-// };
+const ContactList = ({
+  contacts,
+  contactDelete,
+}: {
+  contacts: Contact[];
+  contactDelete: ContactDeleteProp;
+}) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <li key={id} className='flex gap-8'>
+            <span className='pb-3'>
+              {name}: {number}
+            </span>
+            <button
+              className='m-1 bg-[#696969] px-1 text-white hover:bg-black'
+              type='button'
+              onClick={() => contactDelete(id)}
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default ContactList;
